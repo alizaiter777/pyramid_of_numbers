13b161f6b4792b2c2f83974fdaaf6a7b0148b829
alizaiter777 <alizaiter777@gmail.com>
2024-10-28 23:10:10 +0200

    Create exc.py

---
+++ exc.py
@@ -0,0 +1,8 @@
+print("Enter x : ")
+x = int(input())
+#start count from 1 to x which x is the input from user 
+for i in range (1,x+1):
+    #then take i and list all numbers before it presented by j
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print("")
