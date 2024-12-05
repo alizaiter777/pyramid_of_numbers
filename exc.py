59d3a7ce67b37a1f7a1fef0d05319500a380d3e7
alizaiter777 <alizaiter777@gmail.com>
2024-12-05 13:56:24 +0200

    Update exc.py

--- exc.py
+++ exc.py
@@ -1,5 +1,5 @@
-print("Enter x : ")
-x = int(input())
+
+x = int(input("Enter x : "))
 #start count from 1 to x which x is the input from user 
 for i in range (1,x+1):
     #then take i and list all numbers before it presented by j
